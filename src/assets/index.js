d5f3e45eba4fa12f2c6a1718e9ce85ba2d34bc6b
Nikhil Yadav <facts.foundr@gmail.com>
2023-11-13 23:38:29 +0530

    add pngs

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import Product1 from '../assets/1.png';
+import Product2 from '../assets/2.png';
+
+export default {Product1, Product2 };
--- src/pages/shop/Product.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Product = () => {
-  return (
-    <div>This is the products page .</div>
-  )
-}
-
-export default Product;
