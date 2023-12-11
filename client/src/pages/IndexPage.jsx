a327c0d90dc06377071bf6c5ade406d6627a9434
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-11 17:01:04 +0530

    updated login form design

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const IndexPage = () => {
   return (
-    <div className='mt-4 text-center'>
-   This is the Index Page.
+    <div className=' mt-10 py-2 mx-20 border shadow-md border-gray-200 text-black bg-gray-200  rounded-2xl text-center'>
+        <div className=' text-[16px] text-black'>Index Page</div>
 
     
     </div>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useContext, useState } from 'react'
 import { Link, Navigate } from 'react-router-dom';
 import { UserContext } from './../UserContext';
+import { IoLogInOutline } from "react-icons/io5";
 
 const LoginPage = () => {
    const [email, setEmail] =  useState('');
@@ -37,23 +38,30 @@ const LoginPage = () => {
   return (
     // justify-around awesome concept,(for even space on each side)
 
-    <div className='flex mt-4 grow items-center justify-around '>
-        <div className='mb-[22vh]'>
-              <h1 className=' text-3xl text-center mb-4 font-semibold '>Login</h1>
+    <div className='flex mt-4 grow items-center justify-around'>
+        <div className='mb-[22vh]  p-16 border-gray-900 bg-gray-200 shadow-md rounded-2xl'>
+              <div className=' gap-1 inline-flex items-center w-full justify-center text-3xl text-center mb-4 font-semibold '>
+                <IoLogInOutline />
+                <h1 >Login</h1>
+              </div>
               
               <form className=' max-w-sm m-auto text-sm' onSubmit={handleLoginUser}>
                 {/* <input type="text" placeholder='User name' /> */}
                 <input type="email" 
+                 className='shadow-md'
                 placeholder='your @email' 
                 value={email}
                 onChange={ev => setEmail(ev.target.value )}/>
 
                 <input type="password"
+                className='shadow-md'
                  placeholder='your password'
                  value={password}
                  onChange={ev => setPassword(ev.target.value)} />
 
-                <button className='primary'>Login</button>
+                <button className='primary shadow-md border border-gray-100 mt-2'>
+                  Login
+                </button>
 
                 <div className='text-center text-[16px] '>
                   Don't have an account yet ? 
